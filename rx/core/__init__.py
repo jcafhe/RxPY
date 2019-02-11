4e2992f7eb43ab306181f4df5d286c1edeb8bc7c
Jérémie Fache <jeremie.fache@orange.fr>
2019-02-11 18:33:06 +0100

    append declarations from rx/core/__init__.py to rx/internal/__init__.py + remove rx/core/__init__.py

--- rx/core/__init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-from .pipe import pipe
-
-from .observable import Observable, ConnectableObservable
-from .observable import GroupedObservable
-
-from .observer import AnonymousObserver
-from .observer import ObserverBase
--- rx/internal/__init__.py
+++ rx/internal/__init__.py
@@ -1,3 +1,13 @@
+# flake8: noqa
+from .pipe import pipe
+
+from .observable import Observable, ConnectableObservable
+from .observable import GroupedObservable
+
+from .observer import AnonymousObserver
+from .observer import ObserverBase
+
+
 from .priorityqueue import PriorityQueue
 from .basic import noop, default_error, default_comparer
 from .exceptions import SequenceContainsNoElementsError, ArgumentOutOfRangeException, DisposedException
