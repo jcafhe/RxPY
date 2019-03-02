4ac148a5bd5803827ce148d4de5fe12635afc90d
Jérémie Fache <jeremie.fache@orange.fr>
2019-03-02 20:43:54 +0100

    fix qtsheduler leak by removing & deleting qtimer after an action has been invoked or on dispose

--- rx/concurrency/mainloopscheduler/qtscheduler.py
+++ rx/concurrency/mainloopscheduler/qtscheduler.py
@@ -5,7 +5,7 @@ from rx.core import typing
 from rx.disposable import SingleAssignmentDisposable, CompositeDisposable
 from rx.concurrency.schedulerbase import SchedulerBase
 
-log = logging.getLogger("Rx")
+logger = logging.getLogger(__name__)
 
 
 class QtScheduler(SchedulerBase):
@@ -15,38 +15,20 @@ class QtScheduler(SchedulerBase):
         self.qtcore = qtcore
         self._timers = set()
 
-    def _qtimer_schedule(self, time, action, state, periodic=False):
-        scheduler = self
-        msecs = int(self.to_seconds(time)*1000.0)
-
-        sad = SingleAssignmentDisposable()
-
-        periodic_state = [state]
-
-        def interval():
-            if periodic:
-                periodic_state[0] = action(periodic_state[0])
-            else:
-                sad.disposable = action(scheduler, state)
-
-        log.debug("timeout: %s", msecs)
-
-        timer = self.qtcore.QTimer()
-        timer.setSingleShot(not periodic)
-        timer.timeout.connect(interval)
-        timer.setInterval(msecs)
-        timer.start()
-        self._timers.add(timer)
-
-        def dispose():
-            timer.stop()
-            self._timers.remove(timer)
-
-        return CompositeDisposable(sad, Disposable(dispose))
+    def _remove_and_stop_timer(self, qtimer):
+        """
+        Stop (if not already stopped), remove and delete a QTimer object.
+        """
+        try:
+            self._timers.remove(qtimer)
+            qtimer.stop()
+            qtimer.deleteLater()
+        except KeyError:
+            pass
 
     def schedule(self, action: typing.ScheduledAction, state: typing.TState = None) -> typing.Disposable:
         """Schedules an action to be executed."""
-        return self._qtimer_schedule(0, action, state)
+        return self.schedule_relative(0.0, action, state)
 
     def schedule_relative(self, duetime: typing.RelativeTime, action: typing.ScheduledAction,
                           state: typing.TState = None) -> typing.Disposable:
@@ -60,7 +42,26 @@ class QtScheduler(SchedulerBase):
             The disposable object used to cancel the scheduled action
             (best effort).
         """
-        return self._qtimer_schedule(duetime, action, state)
+        scheduler = self
+        msecs = int(self.to_seconds(duetime)*1000.0)
+        sad = SingleAssignmentDisposable()
+
+        def invoke_action():
+            sad.disposable = action(scheduler, state)
+            self._remove_and_stop_timer(timer)
+
+        timer = self.qtcore.QTimer()
+        timer.setSingleShot(True)
+        timer.setInterval(msecs)
+        timer.timeout.connect(invoke_action)
+        self._timers.add(timer)
+        timer.start()
+
+        def dispose():
+            self._remove_and_stop_timer(timer)
+
+        return CompositeDisposable(sad, Disposable(dispose))
+
 
     def schedule_absolute(self, duetime: typing.AbsoluteTime, action: typing.ScheduledAction,
                           state: typing.TState = None) -> typing.Disposable:
@@ -74,9 +75,8 @@ class QtScheduler(SchedulerBase):
             The disposable object used to cancel the scheduled action
             (best effort).
         """
-
-        duetime = self.to_datetime(duetime)
-        return self._qtimer_schedule(duetime, action, state)
+        duetime = self.to_datetime(duetime) - self.now
+        return self.schedule_relative(duetime, action, state)
 
     def schedule_periodic(self, period: typing.RelativeTime, action: typing.ScheduledPeriodicAction,
                           state: typing.TState = None):
@@ -95,4 +95,22 @@ class QtScheduler(SchedulerBase):
             recurring action (best effort).
         """
 
-        return self._qtimer_schedule(period, action, state, periodic=True)
+        msecs = int(self.to_seconds(period)*1000.0)
+        sad = SingleAssignmentDisposable()
+        periodic_state = state
+
+        def interval():
+            nonlocal periodic_state
+            periodic_state = action(periodic_state)
+
+        timer = self.qtcore.QTimer()
+        timer.setSingleShot(False)
+        timer.setInterval(msecs)
+        timer.timeout.connect(interval)
+        timer.start()
+        self._timers.add(timer)
+
+        def dispose():
+            self._remove_and_stop_timer(timer)
+
+        return CompositeDisposable(sad, Disposable(dispose))
